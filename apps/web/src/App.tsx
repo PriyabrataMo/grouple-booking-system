35cdf793645786b4f6d55cc40bbdb8cbc86028d1
Priyabrata <priyabrata8558@gmail.com>
2025-04-15 23:36:47 +0530

    feat: initial commit

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="text-2xl text-center bg-gray-100 min-h-screen flex items-center justify-center">
+      <header className="App-header">
+        <h1>Hello World</h1>
+        <p>Welcome to my React application.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
